b5a05f9116b05463c40abec8a405bddd132794e4
Atiksujon360 <atiksujon7@gmail.com>
2023-10-05 00:23:34 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,101 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addToCartButton = document.getElementById("addToCart");
+  const successModal = document.getElementById("successModal");
+  const closeModalButton = document.getElementById("closeModal");
+  const sizeDropdown = document.getElementById("size");
+  const colorDropdown = document.getElementById("color");
+
+  // Function to update the availability of the "Add to Cart" button
+  function updateAddToCartButton() {
+    const selectedSize = sizeDropdown.value;
+    const selectedOption = sizeDropdown.options[sizeDropdown.selectedIndex];
+
+    // Check if the selected size is available (data-available attribute)
+    const isSizeAvailable =
+      selectedOption.getAttribute("data-available") === "true";
+
+    addToCartButton.disabled = !isSizeAvailable;
+  }
+
+  addToCartButton.addEventListener("click", function () {
+    const selectedSize = sizeDropdown.value;
+    const selectedColor = colorDropdown.value;
+    const selectedQuantity = document.getElementById("quantity").value;
+
+    document.getElementById("selectedSize").textContent = selectedSize;
+    document.getElementById("selectedColor").textContent = selectedColor;
+    document.getElementById("selectedQuantity").textContent = selectedQuantity;
+
+    successModal.style.display = "block";
+  });
+
+  closeModalButton.addEventListener("click", function () {
+    successModal.style.display = "none";
+  });
+
+  // Slider and dots logic
+  const slider = document.querySelector(".slider");
+  const sliderImages = slider.querySelectorAll("img");
+  const dotsContainer = document.querySelector(".dots");
+
+  sliderImages.forEach((image, index) => {
+    const dot = document.createElement("span");
+    dot.classList.add("dot");
+    dot.addEventListener("click", () => {
+      showImage(index);
+    });
+    dotsContainer.appendChild(dot);
+  });
+
+  let currentIndex = 0;
+
+  function showImage(index) {
+    if (index >= 0 && index < sliderImages.length) {
+      slider.style.transform = `translateX(-${index * 100}%)`;
+      currentIndex = index;
+      updateDots();
+    }
+  }
+
+  function updateDots() {
+    const dots = dotsContainer.querySelectorAll(".dot");
+    dots.forEach((dot, index) => {
+      if (index === currentIndex) {
+        dot.classList.add("active");
+      } else {
+        dot.classList.remove("active");
+      }
+    });
+  }
+
+  // Initial price update
+  function updatePrice() {
+    const sizePrice = parseFloat(
+      sizeDropdown.options[sizeDropdown.selectedIndex].getAttribute(
+        "data-price"
+      )
+    );
+    const colorPrice = parseFloat(
+      colorDropdown.options[colorDropdown.selectedIndex].getAttribute(
+        "data-price"
+      )
+    );
+    const totalPrice = sizePrice + colorPrice;
+    document.querySelector(
+      ".product-price"
+    ).textContent = `$${totalPrice.toFixed(2)}`;
+  }
+
+  sizeDropdown.addEventListener("change", function () {
+    updatePrice();
+    updateAddToCartButton();
+  });
+
+  colorDropdown.addEventListener("change", function () {
+    updatePrice();
+  });
+
+  // Initial update of the "Add to Cart" button
+  updateAddToCartButton();
+  updatePrice();
+});
